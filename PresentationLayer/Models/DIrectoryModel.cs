c13e3337230e3ad62251f3b3d2735aa6ed4714aa
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-03 13:13:47 +0800

    presentationLayer

---
+++ PresentationLayer/Models/DIrectoryModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PresentationLayer.Models
+{
+	public class DirectoryViewModel : PageViewModel
+	{
+		public DataLayer.Entities.Directory Directory { get; set; }
+		public List<MaterialViewModel> Materials { get; set; }
+	}
+
+	public class DirectoryEditModel : PageEditModel
+	{
+	}
+}
---
+++ PresentationLayer/Models/MaterialModel.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace PresentationLayer.Models
+{
+	public class MaterialViewModel : PageViewModel
+	{
+		public Material Material { get; set; }
+		public Material NextMaterial { get; set; }
+	}
+	public class MaterialEditModel : PageEditModel
+	{
+		[Required]
+		public int DirectoryId { get; set; }
+	}
+}
